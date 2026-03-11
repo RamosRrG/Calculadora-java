6732b66a81f8c4c0f93a2ddd27ae05985f47b681
Gustavo Ramos ROcha <gustavo.r16@aluno.ifsc.edu.br>
2026-03-11 20:02:58 -0300

    Iniciei o projeto e criei o metodo principal da classe LeitorExpressão

---
+++ src/main/java/main/LeitorExpressao.java
@@ -0,0 +1,29 @@
+package main;
+import java.util.ArrayList;
+
+
+public class LeitorExpressao {
+
+
+    public ArrayList<String> tokens(String expressao) {
+        ArrayList<String> token = new ArrayList<String>();
+        int tamanho = expressao.length();
+        String caracterAtualAcumulado = "";
+        for (int i = 0; i < tamanho; i++) {
+            char caracterAtual = expressao.charAt(i);
+            if (Character.isDigit(caracterAtual)) {
+                caracterAtualAcumulado = caracterAtualAcumulado + caracterAtual;
+            } else if (caracterAtual == '+' || caracterAtual == '-' || caracterAtual == '*' || caracterAtual == '/') {
+                if (!caracterAtualAcumulado.isEmpty()) {
+                    token.add(caracterAtualAcumulado);
+                    caracterAtualAcumulado = "";
+            }token.add(Character.toString(caracterAtual));//quando aplicado o metodo charAt o indice da string virou character. Para armazenar na lista transformei novamente em string.
+
+            }
+        } 
+        if (!caracterAtualAcumulado.isEmpty()) {
+            token.add(caracterAtualAcumulado);}
+        return token;
+    }
+}
+
